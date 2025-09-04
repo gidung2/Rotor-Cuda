ede32caee9fba1aa70d1d42c4da62a4f4794b05c
gidung2 <97887384+gidung2@users.noreply.github.com>
2025-09-05 05:09:31 +0900

    Update Rotor.cpp
    Signed-off-by: gidung2 <97887384+gidung2@users.noreply.github.com>

--- Linux/Rotor.cpp
+++ Linux/Rotor.cpp
@@ -1779,23 +1779,31 @@ void Rotor::Search(int nbThread, std::vector<int> gpuId, std::vector<int> gridSi
 
 								if (avgGpuKeyRate > 1000000000) {
 									memset(timeStr, '\0', 256);
-									printf("\r  [%s] [R: %llu] %s [F: %d] [GPU: %.2f Gk/s] [T: %s]  ",
-										toTimeStr(t1, timeStr),
-										rKeyCount,
-										rhex.GetBase16().c_str(),
-										nbFoundKey,
-										avgGpuKeyRate / 1000000000.0,
-										formatThousands(count).c_str());
+printf("\r  [%s] [R: %llu] %s [F: %d] [GPU: %.2f %s] [T: %s]      ",
+    toTimeStr(t1, timeStr),
+    rKeyCount,
+    rhex.GetBase16().c_str(),
+    nbFoundKey,
+    (avgGpuKeyRate > 1000000000) ? avgGpuKeyRate / 1000000000.0 : avgGpuKeyRate / 1000000.0,
+    (avgGpuKeyRate > 1000000000) ? "Gk/s" : "Mk/s",
+    formatThousands(count).c_str()
+);
+fflush(stdout);
+
 								}
 								else {
 									memset(timeStr, '\0', 256);
-									printf("\r  [%s] [R: %llu] %s [F: %d] [GPU: %.2f Mk/s] [T: %s]  ",
-										toTimeStr(t1, timeStr),
-										rKeyCount,
-										rhex.GetBase16().c_str(),
-										nbFoundKey,
-										avgGpuKeyRate / 1000000.0,
-										formatThousands(count).c_str());
+printf("\r  [%s] [R: %llu] %s [F: %d] [GPU: %.2f %s] [T: %s]      ",
+    toTimeStr(t1, timeStr),
+    rKeyCount,
+    rhex.GetBase16().c_str(),
+    nbFoundKey,
+    (avgGpuKeyRate > 1000000000) ? avgGpuKeyRate / 1000000000.0 : avgGpuKeyRate / 1000000.0,
+    (avgGpuKeyRate > 1000000000) ? "Gk/s" : "Mk/s",
+    formatThousands(count).c_str()
+);
+fflush(stdout);
+
 								}
 							}
 						}
@@ -1929,26 +1937,34 @@ void Rotor::Search(int nbThread, std::vector<int> gpuId, std::vector<int> gridSi
 
 									if (isAlive(params)) {
 										memset(timeStr, '\0', 256);
-										printf("\r  [%s] %s [F: %d] [C: %lf %%] [GPU: %.2f Gk/s] [T: %s]  ",
-											toTimeStr(t1, timeStr),
-											rhex.GetBase16().c_str(),
-											nbFoundKey,
-											completedPerc,
-											avgGpuKeyRate / 1000000000.0,
-											formatThousands(count).c_str());
+printf("\r  [%s] [R: %llu] %s [F: %d] [GPU: %.2f %s] [T: %s]      ",
+    toTimeStr(t1, timeStr),
+    rKeyCount,
+    rhex.GetBase16().c_str(),
+    nbFoundKey,
+    (avgGpuKeyRate > 1000000000) ? avgGpuKeyRate / 1000000000.0 : avgGpuKeyRate / 1000000.0,
+    (avgGpuKeyRate > 1000000000) ? "Gk/s" : "Mk/s",
+    formatThousands(count).c_str()
+);
+fflush(stdout);
+
 									}
 								}
 								else {
 
 									if (isAlive(params)) {
 										memset(timeStr, '\0', 256);
-										printf("\r  [%s] %s [F: %d] [C: %lf %%] [GPU: %.2f Mk/s] [T: %s]  ",
-											toTimeStr(t1, timeStr),
-											rhex.GetBase16().c_str(),
-											nbFoundKey,
-											completedPerc,
-											avgGpuKeyRate / 1000000.0,
-											formatThousands(count).c_str());
+printf("\r  [%s] [R: %llu] %s [F: %d] [GPU: %.2f %s] [T: %s]      ",
+    toTimeStr(t1, timeStr),
+    rKeyCount,
+    rhex.GetBase16().c_str(),
+    nbFoundKey,
+    (avgGpuKeyRate > 1000000000) ? avgGpuKeyRate / 1000000000.0 : avgGpuKeyRate / 1000000.0,
+    (avgGpuKeyRate > 1000000000) ? "Gk/s" : "Mk/s",
+    formatThousands(count).c_str()
+);
+fflush(stdout);
+
 									}
 								}
 							}
